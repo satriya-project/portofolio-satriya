950e3fb2da364ad880f887225f0f447bff8b87f9
satriya-project <wibowosatriyaarif@gmail.com>
2026-05-16 01:54:30 +0700

    update website

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ setTimeout(ketik,200);
 }
 
 ketik();
+const music = new Audio("musik.mp4");
+music.loop = true;
+
+// lanjutkan posisi
+if (localStorage.getItem("musicTime")) {
+  music.currentTime = localStorage.getItem("musicTime");
+}
+
+// simpan posisi terus
+setInterval(() => {
+  localStorage.setItem("musicTime", music.currentTime);
+}, 1000);
+
+// auto play setelah interaksi pertama
+function startMusic() {
+  music.play();
+  document.removeEventListener("click", startMusic);
+}
+
+document.addEventListener("click", startMusic);
