9431b6c1186b512ed6d466a4edb5225393d5902f
satriya-project <wibowosatriyaarif@gmail.com>
2026-05-16 02:03:24 +0700

    update website

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-
-
-const text = 'Satriya.';
+const text = "Satriya.";
 let index = 0;
 
-function ketik(){
-
-if(index < text.length){
-
-document.getElementById('typing').innerHTML += text.charAt(index);
-
-index++;
-
-setTimeout(ketik,200);
-
+function ketik() {
+  if (index < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(ketik, 200);
+  }
 }
 
-}
-
+// jalankan saat halaman selesai load
+window.onload = function () {
+  ketik();
+};
